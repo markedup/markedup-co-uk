7f782befa6ac5b495851609eef76f00132cd3689
Pete Fairhurst <pete.fairhurst@somoglobal.com>
2018-08-08 16:56:58 +0100

    Initial ToDos completed
    Migrated to Webpack process; rewrote animations in pure JS, removed jQuery dependency

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# markedup-co-uk
+# Marked up
+
+Simple placeholder page for my personal domain, based on some (very old) tried and tested code.
+
+## To do
+
+- [x] Migrate to Webpack Dev Server for local development.
+- [x] Bin jQuery and go with pure ES2015+ for scripted effects.
+- [ ] Update layout to use Flexbox or CSS Grid.
+- [ ] Integrate automated deployment service.
+- [ ] Add PostCSS to automagically add relevant CSS vendor variations.
+- [ ] Ensure built static assets are logically organised into discreet, hashed files per type.
+- [ ] Look at integrating [Critters](https://github.com/GoogleChromeLabs/critters) into build process.
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: [`${__dirname}/src/scripts/index.js`],
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/scripts/',
+    filename: 'main.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
